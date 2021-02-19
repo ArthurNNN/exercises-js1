e185a0c075057940435bd17f49159dda9b6822b1
ArthurNNN <nikitinarthur@gmail.com>
2021-02-19 16:59:45 +0100

    finished F-string part of the homework

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
 const name = " Daniel  ";
-
+let nameTrim = name.trim();
+let message = "My name is " + nameTrim + " and my name is " + nameTrim.length + " characters long";
 console.log(message);
+
