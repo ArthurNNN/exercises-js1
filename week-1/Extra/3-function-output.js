8da2332828bc6b24e748d49962af6dc1df054f4a
ArthurNNN <nikitinarthur@gmail.com>
2021-02-20 14:47:47 +0100

    finished 1-4 parts of the Extra tasks

--- week-1/Extra/3-function-output.js
+++ week-1/Extra/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+// function getNumber returns a random number between 0 (inclusive),  and 10 (exclusive)
 function getNumber() {
   return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// Function s returns a join of two strings w1 and w2.
 function s(w1, w2) {
   return w1.concat(w2);
 }
@@ -11,6 +13,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
   // Write the body of this function to concatenate three words together.
   // Look at the test case below to understand what this function is expected to return.
+  return firstWord + " " + secondWord + " " + thirdWord;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
@@ -21,14 +24,14 @@ To run these tests type `node 3-function-output` into your terminal
 const util = require('util');
 
 function test(test_name, actual, expected) {
-    let status;
-    if (actual === expected) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+  let status;
+  if (actual === expected) {
+    status = "PASSED";
+  } else {
+    status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+  }
 
-    console.log(`${test_name}: ${status}`);
+  console.log(`${test_name}: ${status}`);
 }
 
 test(
