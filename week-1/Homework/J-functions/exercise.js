04f684e7b0e44cddcbdb243005d707de685aeaf3
ArthurNNN <nikitinarthur@gmail.com>
2021-02-20 13:54:33 +0100

    finished J-string part of the homework

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,17 @@
 function halve(number) {
   // complete the function here
+
+  return number / 2;
 }
 
 var result = halve(12);
+
+console.log(result);
+
+result = halve(17);
+
+console.log(result);
+
+result = halve(0.5);
 
 console.log(result);
