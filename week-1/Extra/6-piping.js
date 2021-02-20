0c92453f6c1be08631659c2800b85c825be5d32c
ArthurNNN <nikitinarthur@gmail.com>
2021-02-20 15:01:47 +0100

    finished 5-6 Extra  parts of the homework

--- week-1/Extra/6-piping.js
+++ week-1/Extra/6-piping.js
@@ -16,26 +16,31 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+    return a * b ;
 }
 
-function format() {
-
+function format(num) {
+    return "£" + num;
 }
 
 const startingValue = 2
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode = 
+let badCode = format(multiply(add(startingValue, 10), 2));
+
 
 /* BETTER PRACTICE */
 
-let goodCode = 
+var var1 = add(startingValue, 10);
+var var2 = multiply(var1, 2);
+var var3 = format(var2);
+
+let goodCode = var3;
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
@@ -56,9 +61,9 @@ function test(test_name, actual, expected) {
     console.log(`${test_name}: ${status}`);
 }
 
-test('add function - case 1 works', add(1,3), 4)
-test('add function - case 2 works', add(2.4,5), 7.4)
-test('multiply function works', multiply(2,3), 6)
+test('add function - case 1 works', add(1, 3), 4)
+test('add function - case 2 works', add(2.4, 5), 7.4)
+test('multiply function works', multiply(2, 3), 6)
 test('format function works', format(16), "£16")
 test('badCode variable correctly assigned', badCode, "£24")
 test('goodCode variable correctly assigned', goodCode, "£24")
